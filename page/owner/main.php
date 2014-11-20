0db9488cf499fdcb5d35298494124324ba8d4762
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2014-11-20 15:04:52 +0530

    Icon added

--- page/owner/main.php
+++ page/owner/main.php
@@ -2,7 +2,7 @@
 
 class page_extendedImages_page_owner_main extends page_componentBase_page_owner_main {
 	function initMainPage(){
-		$this->add('H3')->setHTML('Component Owner Main Page<small> NO Option At Backend</small>');
+		$this->add('H3')->setHTML('<i class="fa fa-picture-o"></i> '.$this->component_name.'<small> Images with magics</small>');
 	}
 
 
